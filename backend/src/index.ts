b429a9c6be8353b7e09a9d96fd2b41766736c028
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2026-08-03 22:04:48 -0700

    feat: initialized express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Ledger API");
+import express, { type Express, type Request, type Response } from 'express';
+
+const app: Express = express();
+const port = 3000; 
+
+app.get('/', (req: Request, res: Response) => {
+    console.log(app.mountpath);
+    res.send('Welcome again to Ledger Homepage');
+});
+
+app.listen(port, () =>{
+    console.log(`Ledger is listening on Port ${port}`);
+});
